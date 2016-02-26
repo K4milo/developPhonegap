49dc736913a046ac4f6094261fe6dbe156f6e92f
Camilo Andrés Vanegas <kmilo.vanegas@gmail.com>
2016-02-26 12:43:24 -0500

    Create example-json.js

---
+++ example-json.js
@@ -0,0 +1,27 @@
+[{
+    "location": {
+        "latitude": 4.710988599999999,
+        "longitude": -74.072092,
+        "displayAddress": "91,Cl. 128 #71-A,Bogotá,Colombia"
+    },
+    "storeNumber": 1,
+    "name": "Alabaster",
+    "supertarget": false,
+    "services": ["Pharmacy", "Starbucks", "Wine Available", "Flu Shot"],
+    "pharmacy": true,
+    "hours": ["M-Fr: 8:00 a.m.- 10:00 p.m.", "Sa: 8:00 a.m. - 10:00 p.m.", "Su: 8:00 a.m.- 9:00 p.m.", "11/25-12/15 We will open Mon-Sat 8:00AM-11:00PM and Sun 8:00AM-10:00PM."]
+},
+{
+    "location": {
+        "latitude": 4.710988599999999,
+        "longitude": -74.072092,
+        "displayAddress": "AV. Suba - 127D"
+    },
+    "storeNumber": 2,
+    "name": "Alabaster",
+    "supertarget": false,
+    "services": ["Pharmacy", "Starbucks", "Wine Available", "Flu Shot"],
+    "pharmacy": true,
+    "hours": ["M-Fr: 8:00 a.m.- 10:00 p.m.", "Sa: 8:00 a.m. - 10:00 p.m.", "Su: 8:00 a.m.- 9:00 p.m.", "11/25-12/15 We will open Mon-Sat 8:00AM-11:00PM and Sun 8:00AM-10:00PM."]
+}
+]
